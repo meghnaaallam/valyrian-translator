4b228768fab019f4f6b4240a782c32c97573b29b
Meghna Reddy <53735437+meghnareddy1999@users.noreply.github.com>
2022-02-22 21:59:49 +0530

    added app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+var inputText= document.querySelector("#input-area");
+var translateButton= document.querySelector("#btn-translate");
+var translatedText = document.querySelector("#translated-output")
+translateButton.addEventListener("click", myFunction)
+
+function myFunction() {
+  
+console.log("clicked");
+}
