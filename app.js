9093faf2932f337cc1d5cef15da7665ebbb559a6
Meghna Reddy <53735437+meghnareddy1999@users.noreply.github.com>
2022-02-23 12:21:18 +0530

    js:done

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var inputText= document.querySelector("#input-area");
 var translateButton= document.querySelector("#btn-translate");
 var translatedText = document.querySelector("#translated-output")
-translateButton.addEventListener("click", myFunction)
 
-function myFunction() {
-  
-console.log("clicked");
+var serverURL= "https://api.funtranslations.com/translate/valyrian.json";
+ 
+function errorHandler(error) {
+    console.log(error.message);
+    if(error.code=429) {
+        alert("Too Many Requests: Rate limit of 5 requests per hour exceeded. Please wait ")
+    }
+    // alert(error.message)
+    
+}
+
+translateButton.addEventListener("click", myFunction)
+ function myFunction() {
+     displayText = inputText.value;
+     console.log(displayText);
+     constructURL =serverURL+"?text="+ displayText;
+     fetch(constructURL) 
+     .then(response => response.json()) 
+     .then((output) => {
+         var tranlatedText=output.contents.translated;
+     translatedText.innerText=tranlatedText;
+   console.log(tranlatedText);
+     }
+     )
+     .catch(errorHandler)
 }
